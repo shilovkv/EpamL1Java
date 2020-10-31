c8a7098873078b1e1b7a754bcc68c2710c5b0a01
kshilov <kshilov@gmail.com>
2020-10-31 22:13:37 +0300

    Commit with 5th task

---
+++ src/com/myprojct/epam/module1/Ex5.java
@@ -0,0 +1,20 @@
+package com.myprojct.epam.module1;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter time in seconds (T): ");
+        int t = sc.nextInt();
+
+        System.out.println(secondsToTime(t));
+    }
+
+    private static String secondsToTime(int t) {
+        int h = t / 3600;
+        int m = (t - h * 3600) / 60;
+        int s = (t - h * 3600 - m * 60);
+        return String.format("%dч %dмин %dс", h, m, s);
+    }
+}
