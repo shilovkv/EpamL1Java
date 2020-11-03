890defce10f36373fec6fae2692b2e73ce941775
kshilov <kshilov@gmail.com>
2020-11-03 23:41:40 +0300

    Commit with module 2 3rd task

---
+++ src/com/myprojct/epam/module2/Ex3.java
@@ -0,0 +1,24 @@
+package com.myprojct.epam.module2;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter x1 and y1:");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        System.out.print("Please enter x2 and y2:");
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        System.out.print("Please enter x3 and y3:");
+        int x3 = scanner.nextInt();
+        int y3 = scanner.nextInt();
+
+        if ((y3 - y1) * (x2 - x1) == (y2 - y1) * (x3 - x1)) {
+            System.out.println("Points are on one line");
+        } else {
+            System.out.println("Points are NOT on one line");
+        }
+    }
+}
