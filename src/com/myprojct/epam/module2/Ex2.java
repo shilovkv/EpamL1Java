ab3572cc1db7f2fbc6d0b64464903b4e76ff2d6b
kshilov <kshilov@gmail.com>
2020-11-03 23:25:07 +0300

    Commit with module 2 2nd task

---
+++ src/com/myprojct/epam/module2/Ex2.java
@@ -0,0 +1,36 @@
+package com.myprojct.epam.module2;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a, b, c and d: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        System.out.println(max(min(a, b), min(c, d)));
+    }
+
+    /**
+     * method returns ~max~ of two int numbers
+     * @param a first int number
+     * @param b second int number
+     * @return max of two int numbers
+     */
+    private static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    /**
+     * method returns ~min~ of two int numbers
+     * @param a first int number
+     * @param b second int number
+     * @return min of two int numbers
+     */
+    private static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
