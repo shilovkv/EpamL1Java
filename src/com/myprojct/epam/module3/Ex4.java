c6a3e2de3e5239ea85391321287c3857100091b0
Kirill Shilov <kshilov@gmail.com>
2020-11-04 20:52:19 +0300

    Commit with module 3 task 4

---
+++ src/com/myprojct/epam/module3/Ex4.java
@@ -0,0 +1,13 @@
+package com.myprojct.epam.module3;
+
+import java.math.BigInteger;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        BigInteger mult = new BigInteger("1");
+        for (int i = 1; i <= 200; i++) {
+            mult = mult.multiply(BigInteger.valueOf(i * i));
+        }
+        System.out.printf("The answer is: %d", mult);
+    }
+}
