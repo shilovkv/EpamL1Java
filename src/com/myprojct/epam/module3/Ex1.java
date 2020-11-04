2047c208de961c196560d4a4b65bebb57eb396ff
Kirill Shilov <kshilov@gmail.com>
2020-11-04 19:16:10 +0300

    Commit with module 3 task 1

---
+++ src/com/myprojct/epam/module3/Ex1.java
@@ -0,0 +1,21 @@
+package com.myprojct.epam.module3;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter any positive integer: ");
+        int x = scanner.nextInt();
+
+        System.out.printf("The sum from 1 to %d is: %d", x, sum(x));
+    }
+
+    private static int sum(int x) {
+        int sum = 0;
+        for (int i = 1; i <= x; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
