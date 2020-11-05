299f6fd6c3945e3572be9377f1375c7a24266b6f
kshilov <kshilov@gmail.com>
2020-11-05 23:04:18 +0300

    Commit with module 2 6th task

---
+++ src/com/myprojct/epam/module3/Ex6.java
@@ -0,0 +1,10 @@
+package com.myprojct.epam.module3;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        char c = 0;
+        do {
+            System.out.println(c + " " + (int)c);
+        } while (++c != 0);
+    }
+}
