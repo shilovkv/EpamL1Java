6966dc0ae793aea37cac92d369c1db1353997af4
Kirill Shilov <kshilov@gmail.com>
2020-11-08 17:09:16 +0300

    Commit with module 3 task 8

---
+++ src/com/myprojct/epam/module3/Ex7.java
@@ -0,0 +1,26 @@
+package com.myprojct.epam.module3;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter m and n: ");
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        for (int i = m; i <= n; i++) {
+            System.out.print(i + ": ");
+            divider(i);
+            System.out.println();
+        }
+    }
+
+    private static void divider(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
---
+++ src/com/myprojct/epam/module3/Ex8.java
@@ -0,0 +1,36 @@
+package com.myprojct.epam.module3;
+
+import java.util.Scanner;
+
+public class Ex8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" Please enter 2 integers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + (getArrayOfNumbers(a)[i] && getArrayOfNumbers(b)[i]));
+        }
+    }
+
+    /**
+     * finding all numbers in integer provided
+     * @param a an integer to find all numbers in it
+     * @return boolean[10] where [0] is for number 0 (true if a contains any 0s, false if not); [1] for 1 ... [9] for 9
+     */
+    private static boolean[] getArrayOfNumbers(int a) {
+        boolean[] numbers = new boolean[10];
+
+        if (a == 0) {
+            numbers[0] = true;
+        } else {
+            while (a != 0) {
+                numbers[a % 10] = true;
+                a /= 10;
+            }
+        }
+
+        return numbers;
+    }
+}
